a62fecdacd62fda81791cb1b71693b2f80544a6d
yevgnen <wherejoystarts@gmail.com>
2016-02-02 14:34:40 +0800

    Add logtistic Regression.

--- ch04/logistic_regression.py
+++ ch04/logistic_regression.py
@@ -3,31 +3,40 @@
 
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy import linalg as linalg
+from scipy.special import expit
 
 
 def phi(x):
     return x
 
 
-def percetpron(x, t, w):
+def logistic_regression(x, t, w, eps=1e-2, max_iter=int(1e3)):
     N = x.shape[1]
-    x = np.vstack([np.ones(N), phi(x)])
-    correct = 0
-    while (correct < N):
-        for i in range(N):
-            xn = x[:, i]
-            tn = t[i]
-            if (w.T.dot(xn) * tn < 0):
-                w = w + xn * tn
-                correct = 0
-                break
-            else:
-                correct += 1
+    Phi = np.vstack([np.ones(N), phi(x)]).T
+
+    for k in range(max_iter):
+        y = expit(Phi.dot(w))
+        R = np.diag(np.ones(N) * (y * (1 - y)))
+        H = Phi.T.dot(R).dot(Phi)
+        g = Phi.T.dot(y - t)
+
+        w_new = w - linalg.inv(H).dot(g)
+
+        diff = linalg.norm(w_new - w) / linalg.norm(w)
+        if (diff < eps):
+            break
+
+        w = w_new
+        k = k + 1
+
+        print("{0:5d} {1:10.6f}".format(k, diff))
+
     return w
 
 
 def sample(N, label=np.array([1, 0]), mean1=np.array([-1, 1]), mean2=np.array([1, -1]),
-           cov=np.array([[1, 0.5], [0.5, 1]])):
+           cov=np.array([[0.5, 0.3], [0.3, 0.5]])):
     M = int(N / 2)
     c1 = np.random.multivariate_normal(mean1, cov, M)
     c2 = np.random.multivariate_normal(mean2, cov, M)
@@ -40,10 +49,10 @@ def sample(N, label=np.array([1, 0]), mean1=np.array([-1, 1]), mean2=np.array([1
 
 def main():
     N = 500
-    x, t = sample(N, label=np.array([-1, 1]), cov=np.array([[0.2, 0.1], [0.1, 0.2]]))
+    x, t = sample(N, cov=np.array([[0.8, 0.2], [0.2, 0.8]]))
 
     w = np.array([1.0, 1.0, 1.0])
-    w = percetpron(x, t, w)
+    w = logistic_regression(x, t, w)
 
     x1 = np.linspace(x[0, :].min() - 1, x[0, :].max() + 1, 500)
     plt.figure()
