15314a7671d3a4e821a3f8345d29cf9db6db18b5
yevgnen <wherejoystarts@gmail.com>
2016-02-11 19:14:44 +0800

    Add Bayesian Linear Regression.

---
+++ ch03/bayesian_linear_regression.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from matplotlib import pyplot as plt
+from regression_sample import RegressionSample
+from scipy.stats import norm as norm_dist
+from scipy import linalg as linalg
+from linear_model import BayesianLinearRegression, LinearLeastSquare
+
+
+def main():
+
+    # Generate training data
+    N = 25
+    f = lambda x: np.sin(2 * np.pi * x).flatten()
+    sigma = 0.2
+    sample = RegressionSample(f, N, sigma=sigma)
+    x = sample.x
+    t = sample.t
+
+    # Geenerate test data
+    x_min, x_max = x.min(), x.max()
+    t_min, t_max = t.min(), t.max()
+    x_test = np.array([np.linspace(x_min, x_max, 200)]).T
+    t_test = f(x_test)
+
+    # Construct basis functions
+    basis_functions = []
+    locs = np.linspace(x.min(), x.max(), 9)
+    print(locs)
+    for loc in locs:
+        basis = (
+            lambda loc: (lambda x: np.e**(-(linalg.norm(x - loc)**2) / (2 * 1e-2)))
+        )(loc)
+        basis_functions.append(basis)
+    n_basis = len(basis_functions)
+
+    # Model fitting
+    alpha = 2.0
+    prior_cov = np.eye(n_basis) / alpha
+    prior_mean = np.zeros(n_basis)
+    blr = BayesianLinearRegression(basis_functions=basis_functions,
+                                   mean=prior_mean,
+                                   cov=prior_cov)
+
+    # Sequential learning
+    beta = (1 / sigma)**2
+    for i in range(N):
+        blr.fit(x[i, :], t[i], beta)
+
+    # Prediction
+    predictions = blr.predict(x_test)
+
+    plt.figure()
+    plt.scatter(x, t, marker='x')
+    plt.plot(x_test[:, 0], t_test, 'r-')
+    plt.plot(x_test[:, 0], predictions, 'g-')
+
+    for i, basis in enumerate(basis_functions):
+        plt.plot(x_test[:, 0], blr.mean[i] *
+                 np.array([basis(x) for x in x_test[:, 0]]), 'm--')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
--- ch03/linear_model.py
+++ ch03/linear_model.py
@@ -44,7 +44,7 @@ class LinearLeastSquare(object):
     def predict(self, X):
         Phi = self.nonlinear_transformation(X)
 
-        if (self.w.ndim == 1):
+        if (self.w.ndim < 2):
             y = self.w.dot(Phi.T).flatten()
         else:
             y = Phi.dot(self.w)  # self.w.T.dot(Phi.T).T
@@ -52,3 +52,52 @@ class LinearLeastSquare(object):
         return y
 
 
+class BayesianLinearRegression(object):
+    def __init__(self, basis_functions=None, mean=0, cov=1):
+        if (basis_functions == None):
+            raise Exception('Please privode basis functions!')
+
+        self.basis_functions = basis_functions
+        self.n_basis = len(self.basis_functions)
+        self.mean = mean
+        self.cov = cov
+
+        return
+
+    def nonlinear_transformation(self, x):
+        if (x.ndim < 2):
+            return np.array([phi(x) for phi in self.basis_functions])
+        else:
+            n_sample = x.shape[0]
+            phi = np.zeros((n_sample, self.n_basis))
+            for i in range(n_sample):
+                phi[i, :] = self.nonlinear_transformation(x[i, :])
+
+            return phi
+
+    def fit(self, X, T, beta):
+        Phi = self.nonlinear_transformation(X)
+        PhiTPhi = np.outer(Phi, Phi) if Phi.ndim < 2 else Phi.T.dot(Phi)
+
+        prior_mean = self.mean
+        prior_cov = self.cov
+        inv_prior_cov = linalg.inv(prior_cov)
+
+        post_cov = linalg.inv(inv_prior_cov + beta * PhiTPhi)
+        post_mean = post_cov.dot(inv_prior_cov.dot(prior_mean) + beta *
+                                 Phi.T.dot(T))
+
+        print(post_mean)
+        self.mean = post_mean
+        self.cov = post_cov
+
+    def predict(self, X):
+        Phi = self.nonlinear_transformation(X)
+
+        w = self.mean
+        if (w.ndim < 2):
+            y = w.dot(Phi.T).flatten()
+        else:
+            y = Phi.dot(w)
+
+        return y
