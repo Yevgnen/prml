701138c4896ca23f274efeb12171c1f844cc8d36
yevgnen <wherejoystarts@gmail.com>
2016-02-28 20:59:34 +0800

    Visualization for classification.

--- Ch05 - Neural Networks/neural_network_classification.py
+++ Ch05 - Neural Networks/neural_network_classification.py
@@ -20,9 +20,8 @@ max_iter = int(1e3)
 tol = 1e-5
 
 nn = NeuralNetwork(
-    [Layer('Tanh', (2, 3)),
-     Layer('Sigmoid', (3, 4)),
-     Layer('Tanh', (4, 1))], max_iter=max_iter, tol=tol)
+    [Layer('Tanh', (2, 5)),
+     Layer('Tanh', (5, 1))], max_iter=max_iter, tol=tol)
 nn.fit(X_train, T_train)
 
 # Predcition
@@ -35,6 +34,15 @@ print(score)
 plt.scatter(X_train[:, 0],
             X_train[:, 1],
             s=40,
+            zorder=10,
             c=T_train,
-            cmap=plt.cm.Spectral)
-# plt.show()
+            cmap=plt.cm.Paired)
+
+h = 0.01
+x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
+y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z = nn.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+plt.contourf(xx, yy, Z, cmap=plt.cm.Paired)
+plt.show()
