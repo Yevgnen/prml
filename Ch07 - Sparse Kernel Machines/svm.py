SVM fixed the wrong computation of `b`.

Ch07 - Sparse Kernel Machines/svm.py
@@ -5,50 +5,51 @@ import numpy as np
 import scipy.linalg as linalg
 from scipy.optimize import minimize as minimize
 
-from cvxopt import matrix, solvers
-from kernel import GaussianKernel
+from kernel import GaussianKernel, LinearKernel
 
 
 class SVM(object):
-    def __init__(self):
-        self.kernel = GaussianKernel()
+    def __init__(self, kernel='gaussian'):
+        self.supported_kernel = {
+            'gaussian': GaussianKernel(),
+            'linear': LinearKernel()
+        }
+        self.kernel = self.supported_kernel[kernel]
 
-    def fit(self, X, T, max_iter=int(1e3), tol=1e-5):
+    def fit(self, X, T, C=np.inf, max_iter=int(1e3), tol=1e-5):
         """Use training data ``X`` and ``T`` to fit a SVM models. """
 
         n_samples = X.shape[0]
+        # Compute the Gram matrix of training data
         K = self.kernel.inner(X, X)
 
         # The target function: (1/2)*x'*Q*x + p'*x
         Q = T.reshape(1, -1) * K * T.reshape(-1, 1)
         p = -np.ones(n_samples)
-        # The inequality constrains: G(x) >= 0
-        G = np.eye(n_samples)
-        # The equality constrains: H(x) = 0
-        A = T
-
         lagrange = lambda x: (0.5 * x.dot(Q).dot(x) + p.dot(x), Q.dot(x) + p)
-        cons = ({'type': 'ineq',
-                 'fun': lambda x: G.dot(x),
-                 'jac': lambda x: G},
-                {'type': 'eq',
+
+        # The equality constraints: H(x) = 0
+        A = T
+        cons = ({'type': 'eq',
                  'fun': lambda x: A.dot(x),
                  'jac': lambda x: A})
 
-        # solve the quadratic program
+        # The inequality constaints: 0 <= G(x) <= C
+        bnds = [(0, C) for i in range(n_samples)]
+
+        # Solve the quadratic program
         opt_solution = minimize(lagrange,
-                                # a,
                                 np.zeros(n_samples),
                                 method='SLSQP',
                                 constraints=cons,
+                                bounds=bnds,
                                 tol=tol,
                                 jac=True,
                                 options={'maxiter': max_iter,
                                          'disp': True})
 
         self.dual_var = opt_solution.x
-        self.support_vector_indices = np.nonzero(1 - np.isclose(self.dual_var, 0))[0]
-        print(self.support_vector_indices.size)
+        self.sv_indices = np.nonzero(1 - np.isclose(self.dual_var, 0))[0]
 
         return self
 
@@ -59,17 +60,20 @@ class SVM(object):
         Y = np.zeros(n_tests)
 
         dual_var = self.dual_var
-        S = self.support_vector_indices
+        sv_indices = self.sv_indices
 
-        sub_dual_var = dual_var[S]
-        sub_X = X[S]
-        sub_T = T[S]
+        sv_dual_var = dual_var[sv_indices]
+        sv_X = X[sv_indices]
+        sv_T = T[sv_indices]
 
-        K = self.kernel.inner(sub_X, sub_X)
-        b = 1 / S.size * (sub_T.sum() - sub_dual_var.dot(K).dot(sub_T))
+        K = self.kernel.inner(sv_X, sv_X)
+        # b = 1 / sv_indices.size * (sv_T.sum() - sv_dual_var.dot(K).dot(sv_T))
+        b = 1 / sv_indices.size * (sv_T.sum() - K.dot(sv_dual_var * sv_T).sum())
 
-        Y = (sub_T * sub_dual_var).dot(self.kernel.inner(sub_X, X_new)) + b
+        Y = (sv_T * sv_dual_var).dot(self.kernel.inner(sv_X, X_new)) + b
+        YY = (sv_T * sv_dual_var).dot(self.kernel.inner(sv_X, X[self.sv_indices])) + b
 
+        # import ipdb; ipdb.set_trace()
         Y[Y > 0] = 1
         Y[Y < 0] = -1
 

Ch07 - Sparse Kernel Machines/svm_classification.py
@@ -4,42 +4,11 @@ from sklearn import datasets
 
 from svm import SVM
 
-
 # Generate dataset
-# n_samples = 40
-# sigma = 2e-2
-# X, T = datasets.make_moons(n_samples, noise=sigma)
-
-class ClassificationSample(object):
-    def __init__(self,
-                 N=100,
-                 K=2,
-                 mean=np.array([[1, -1], [-1, 1]]),
-                 cov=np.array([[0.2, 0.1], [0.1, 0.2]])):
-        n = int(N / K)
-        X = np.zeros((N, 2))
-        T = np.zeros((N), dtype=int)
-        label = np.arange(K)
-        pos = 0
-        for k in range(K):
-            X[pos:pos + n, :] = np.random.multivariate_normal(mean[k, :], cov, n)
-            T[pos:pos + n] = label[k]
-            pos = pos + n
-        p = np.random.permutation(N)
-        self.X = X[p, :]
-        self.T = T[p]
-
-N = 100
-n_features = 2
-n_classes = 2
-K = n_classes
-mean = np.array([[5, 0], [0, 5]])
-cov = np.array([[2, -1], [1, 1]])
-sample = ClassificationSample(N, K, mean, cov)
-X = sample.X
-T = sample.T
-T[T==0] = -1
-n_samples = N
+n_samples = 100
+sigma = 2e-2
+X, T = datasets.make_circles(n_samples, noise=sigma, factor=0.4)
+T[T == 0] = -1
 
 half = int(n_samples / 2)
 X_train = X[0:half, :]
@@ -48,11 +17,8 @@ X_test = X[half:, :]
 T_test = T[half:]
 
 # Fit the neural network
-max_iter = int(1e3)
-tol = 1e-5
-
-classifier = SVM()
-classifier.fit(X_train, T_train)
+classifier = SVM(kernel='gaussian')
+classifier.fit(X_train, T_train, C=np.inf)
 
 # Predcition
 prediction = classifier.predict(X_train, T_train, X_test)
@@ -76,8 +42,8 @@ plt.scatter(X_test[:, 0],
             c=T_test,
             cmap=plt.cm.Paired)
 
-svs = X_train[classifier.support_vector_indices]
-svst = T_train[classifier.support_vector_indices]
+svs = X_train[classifier.sv_indices]
+svst = T_train[classifier.sv_indices]
 
 plt.scatter(svs[:, 0],
             svs[:, 1],